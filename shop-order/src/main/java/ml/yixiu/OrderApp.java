3240d9183856bd2f444b340a79d1e030c140ca80
jmme <jmme@foxmail.com>
2022-04-22 17:17:45 +0800

    修改resttemplet

--- shop-order/src/main/java/ml/yixiu/OrderApp.java
+++ shop-order/src/main/java/ml/yixiu/OrderApp.java
@@ -3,12 +3,14 @@ package ml.yixiu;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class OrderApp {
     public static void main(String[] args) {
         SpringApplication.run(OrderApp.class, args);
--- shop-order/src/main/java/ml/yixiu/contorller/OrderController.java
+++ shop-order/src/main/java/ml/yixiu/contorller/OrderController.java
@@ -6,10 +6,14 @@ import ml.yixiu.domain.Order;
 import ml.yixiu.domain.Product;
 import ml.yixiu.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -20,10 +24,19 @@ public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping("/order/prod/{pid}")
     public Order order(@PathVariable("pid") Integer pid){
         log.info("order prod id: {}", pid);
-        Product product = restTemplate.getForObject("http://localhost:8081/product/" + pid, Product.class);
+//        Product product = restTemplate.getForObject("http://localhost:8081/product/" + pid, Product.class);
+        //修改为nacos注册中心获取服务
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("service-product");
+        ServiceInstance instance = serviceInstances.get(0);
+        Product product = restTemplate.getForObject("http://"+instance.getHost()+":"+instance.getPort()+"/product/" + pid, Product.class);
+
         log.info("product: {}", JSON.toJSONString(product));
         Order order = new Order();
 //        order.setUid(1);
